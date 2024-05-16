65ffb79df6da18ae17d30bddf3b242fd5cdeb9b2
TomasCardoso46 <tomascardoso4633@gmail.com>
2024-05-16 12:53:24 +0100

    Changed a bit of bullet code

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -17,28 +17,4 @@ public class Bullet : MonoBehaviour
     {
         transform.Translate(Vector3.left * speed * Time.deltaTime);
     }
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        // Check if the collided object has the tag "Player"
-        if (other.CompareTag("Player"))
-        {
-            // Increase strikes in the GameManager
-            GameManager.instance.health--;
-
-            // If strikes reach 3, destroy the object
-            if (Player.instance.health <= 0)
-            {
-                DestroyPlayer();
-            }
-        }
-    }
-    void DestroyPlayer()
-    {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-
-        if (player != null)
-        {
-            Destroy(player);
-        }
-    }
 }

200b30653cf5d8c024f5b3a8b929ec6b0bb1b780
Leonardo Henrique <leonardojose@netcabo.pt>
2024-05-16 09:56:40 +0100

    Hp management

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance; // Singleton instance
 
-    public int health = 100; // Player health
+    public int health = 0; // Player health
     public int strikes = 0; // Number of strikes
     public SpriteRenderer playerSpriteRenderer;
     public GameObject playerPrefab;
@@ -22,6 +22,19 @@ public class GameManager : MonoBehaviour
         else
         {
             Destroy(gameObject); // Destroy duplicate instances
+        }
+        if (health == 3)
+        {
+            DestroyPlayer();
+        }
+    }
+    void DestroyPlayer()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player != null)
+        {
+            Destroy(player);
         }
     }
 }

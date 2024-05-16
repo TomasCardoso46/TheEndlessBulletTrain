181136d2b939ef46f3daf3536f77630cccc35217
TomasCardoso46 <tomascardoso4633@gmail.com>
2024-05-16 10:39:30 +0100

    Added enemy spawners (still not working)

--- Assets/Scripts/YakuzaSpawner.cs
+++ Assets/Scripts/YakuzaSpawner.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class YakuzaSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject SpawnPoint1;
+    public GameObject SpawnPoint2;
+    public GameObject objectToSpawn;
+    private GameObject SpawnPoint;
+    [SerializeField]
+    private int numberOfEnemies = 0;
+    private bool startSpawn = false;
+    private bool leftSpawner = false;
+    // Update is called once per frame
     void Start()
     {
-        
+        SpawnPoint = SpawnPoint1;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (numberOfEnemies <=6 && startSpawn == true)
+        {
+            StartEnemyTimer();
+        }
     }
+    public void SpawnObject()
+    {
+
+        Instantiate(objectToSpawn, SpawnPoint.transform.position, Quaternion.identity);
+        numberOfEnemies++;
+    }
+    IEnumerator WaitForEnemies()
+    {
+        yield return new WaitForSeconds(1.5f);
+        SpawnObject();
+        SwitchSpawnPoint();
+    }
+    public void StartEnemyTimer()
+    {
+        StartCoroutine(WaitForEnemies());
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the collided object has the tag "Player"
+        if (other.CompareTag("Player"))
+        {
+            startSpawn = true;
+        }
+    }
+    void SwitchSpawnPoint()
+    {
+        if (leftSpawner)
+        {
+            SpawnPoint = SpawnPoint2;
+            leftSpawner = false;
+
+        }
+        else
+        {
+            SpawnPoint = SpawnPoint1;
+            leftSpawner = true;
+        }
+    }
+
+
 }

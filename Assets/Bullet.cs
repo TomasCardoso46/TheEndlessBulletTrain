eb104c56524d06616340b354873bcc01be9e9125
Leonardo Henrique <leonardojose@netcabo.pt>
2024-05-23 11:57:41 +0100

    Player dies from bullets

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -17,4 +17,19 @@ public class Bullet : MonoBehaviour
     {
         transform.Translate(Vector3.left * speed * Time.deltaTime);
     }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log($"Triggered with: {other.name}"); // Log which object is triggering the event
+
+        // If the object collides with something tagged "ParryZone," destroy this GameObject
+        if (other.CompareTag("ParryZone"))
+        {
+            Debug.Log("Contact with ParryZone detected.");
+            Destroy(gameObject);
+        }
+        else
+        {
+            Debug.Log("No ParryZone tag detected.");
+        }
+    }
 }

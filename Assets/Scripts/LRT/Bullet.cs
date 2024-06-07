09ab53bd5cc0104d3377b8fe3ddc91ddb382cea1
tomascardoso4633@gmail.com <tomascardoso.dev@gmail.com>
2024-06-07 23:12:15 +0100

    Finished LTR mechanics

--- Assets/Scripts/LRT/Bullet.cs
+++ Assets/Scripts/LRT/Bullet.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] 
+    [SerializeField]
     private float speed = 5f;
+    private Vector3 playerDirection;
+
     // Start is called before the first frame update
     void Start()
     {
+        // Calculate the direction towards the player
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            playerDirection = (player.transform.position - transform.position).normalized;
+        }
+        else
+        {
+            // If player is not found, shoot the bullet to the left by default
+            playerDirection = Vector3.left;
+        }
         Destroy(gameObject, 3f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        // Move the bullet in the direction of the player
+        transform.Translate(playerDirection * speed * Time.deltaTime);
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log($"Triggered with: {other.name}"); // Log which object is triggering the event
@@ -35,6 +50,6 @@ public class Bullet : MonoBehaviour
         {
             Debug.Log("No ParryZone tag detected.");
         }
-        
+
     }
 }
--- Assets/Scripts/LRT/MarksmanMovement.cs
+++ Assets/Scripts/LRT/MarksmanMovement.cs
@@ -18,6 +18,7 @@ public class MarksmanMovement : MonoBehaviour
     public float fireRateThreshold = 1.5f;
     [SerializeField]
     public int shotsFired = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -35,9 +36,21 @@ public class MarksmanMovement : MonoBehaviour
     {
         if (playerTransform != null)
         {
-
             Vector3 direction = playerTransform.position - transform.position;
             float distance = direction.magnitude;
+
+            // Flip the shooter to face the player along the X-axis
+            if (direction.x > 0)
+            {
+                
+                transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+            else
+            {
+                
+                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+
             if (distance > followDistance)
             {
                 direction.Normalize();
@@ -56,30 +69,29 @@ public class MarksmanMovement : MonoBehaviour
                     shotsFired++;
                     return;
                 }
-
             }
+
             if (shotsFired == 6)
             {
                 fireRateThreshold = 3;
             }
-            else if (shotsFired == 7) 
+            else if (shotsFired == 7)
             {
                 fireRateThreshold = 1.5f;
                 shotsFired = 1;
             }
-                
-
-
         }
-        void ShootPlayer()
-        {
+    }
 
-            Instantiate(bullet, SpawnPoint.position, Quaternion.identity);
+    void ShootPlayer()
+    {
+        Vector3 shootDirection = (playerTransform.position - SpawnPoint.position).normalized;
+        GameObject bulletInstance = Instantiate(bullet, SpawnPoint.position, Quaternion.identity);
+        bulletInstance.GetComponent<Rigidbody2D>().velocity = shootDirection * speed;
+    }
 
-        }
-        void TimeIncrease()
-        {
-            fireRateBom += Time.deltaTime;
-        }
+    void TimeIncrease()
+    {
+        fireRateBom += Time.deltaTime;
     }
 }
